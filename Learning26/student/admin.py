00347312bcaaa332aae663f9b4eda276b7e2c05c
Pradeep Prajapati <pp136859@gmail.com>
2026-02-11 09:43:33 +0530

    06/02/2026- normalization in tables

--- Learning26/student/admin.py
+++ Learning26/student/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import student,products
+from .models import student,StudentProfile,Service,Category
 
 admin.site.register(student)
-admin.site.register(products)
+admin.site.register(StudentProfile)
+admin.site.register(Service)
+admin.site.register(Category)
 # Register your models here.
--- Learning26/student/models.py
+++ Learning26/student/models.py
@@ -9,14 +9,48 @@ class student(models.Model):
 
     class Meta:
         db_table ='student'
+    def __str__(self):
+        return self.studentName
 
-class products(models.Model):
-    productName = models.CharField(max_length=100)
-    productPrice = models.IntegerField()
-    productDescription = models.TextField()
-    productStock = models.PositiveIntegerField()
-    productColor = models.CharField(max_length=20,null=True)
-    productStatus = models.BooleanField(default=True)
+
+class StudentProfile(models.Model):
+    hobbies =(("reading","reading"),("travel","travel"),("music","music"))
+    studentId = models.OneToOneField(student,on_delete=models.CASCADE)
+    studentHobbies = models.CharField(max_length=100,choices=hobbies)
+    studentAddress = models.CharField(max_length=100)
+    studentPhone = models.CharField(max_length=10)
+    studentGender = models.CharField(max_length=10)
+    studentDOB = models.DateField()
     
     class Meta:
-        db_table = "product"
+        db_table = "studentprofile"
+
+    def __str__(self):
+        return self.studentId.studentName    
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    categoryDescription = models.TextField()
+    categoryStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.categoryName  
+
+class Service(models.Model):
+    serviceName = models.CharField(max_length=100)
+    serviceDescription = models.TextField()
+    servicePrice = models.IntegerField()
+    serviceStatus = models.BooleanField(default=True)
+    #after table creation adding new field
+    discount = models.IntegerField(null=True)
+    categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    
+    class Meta:
+        db_table = "service"
+
+    def __str__(self):
+        return self.serviceName    
